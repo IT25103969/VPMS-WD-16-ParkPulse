bf52a8c692860e29a5b2acd3873675094e20b195
IT25103660 <it25103660@my.sliit.lk>
2026-05-14 11:17:30 +0530

    Delete src/main directory

--- src/main/java/com/parking/management/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parking.management.repository;
-
-import com.parking.management.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-    List<Ticket> findByStatus(String status);
-}
