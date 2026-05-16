34243f1e47aa8ce9b63e3e4617da02ac95cb3ac2
IT25102451 <it25102451@my.sliit.lk>
2026-05-16 16:11:51 +0530

    Delete src/main directory

--- src/main/java/com/parking/management/model/MemberType.java
+++
@@ -1,5 +0,0 @@
-package com.parking.management.model;
-
-public enum MemberType {
-    REGULAR, PREMIUM, VIP
-}
--- src/main/java/com/parking/management/model/SlotType.java
+++
@@ -1,5 +0,0 @@
-package com.parking.management.model;
-
-public enum SlotType {
-    CAR, MOTORBIKE, VAN, TRUCK
-}
