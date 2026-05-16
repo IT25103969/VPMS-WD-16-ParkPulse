989de1a47e04fbd925c061ba7e8b84bb68bb9c1d
IT25102451 <it25102451@my.sliit.lk>
2026-05-16 23:45:36 +0530

    Member API

---
+++ src/main/java/com/vpms/membermanagement/controller/MemberController.java
@@ -0,0 +1,40 @@
+package com.vpms.membermanagement.controller;
+
+import com.vpms.membermanagement.model.Member;
+import com.vpms.membermanagement.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+@CrossOrigin(origins = "*") // In production, replace with specific origin
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping
+    public List<Member> getAllMembers(@RequestParam(required = false) String search) {
+        if (search != null && !search.isEmpty()) {
+            return memberService.searchMembers(search);
+        }
+        return memberService.getAllMembers();
+    }
+
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    @PutMapping("/{id}")
+    public Member updateMember(@PathVariable String id, @RequestBody Member member) {
+        return memberService.updateMember(id, member);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMember(@PathVariable String id) {
+        memberService.deleteMember(id);
+    }
+}
